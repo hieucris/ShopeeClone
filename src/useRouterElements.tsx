3da74923ea0df603b59f0094f7182416192c6f92
Van Hieu <hieuhk195@gmail.com>
2023-02-02 10:12:05 +0700

    setup react router

---
+++ src/useRouterElements.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useRoutes } from 'react-router-dom'
+import RegisterLayout from './layouts/RegisterLayout'
+import Login from './pages/Login'
+import ProductList from './pages/ProductList'
+import Register from './pages/Register'
+
+export default function useRouterElements() {
+  const routeElements = useRoutes([
+    {
+      path: '/',
+      element: <ProductList />
+    },
+    {
+      path: '/register',
+      element: (
+        <RegisterLayout>
+          <Register />
+        </RegisterLayout>
+      )
+    },
+    {
+      path: 'login',
+      element: (
+        <RegisterLayout>
+          <Login />
+        </RegisterLayout>
+      )
+    }
+  ])
+  return routeElements
+}
