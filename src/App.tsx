d59306c6e52b4778e04b32aba4f4f061c386c62d
Van Hieu <hieuhk195@gmail.com>
2023-02-07 22:49:30 +0700

    error message

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import userRouterElements from './useRouterElements'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   const routeElements = userRouterElements()
-  return <div>{routeElements}</div>
+  return (
+    <div>
+      {routeElements}
+      <ToastContainer />
+    </div>
+  )
 }
 
 export default App
